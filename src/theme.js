b9c1c5db1e5500b740b53723ec23544f6dcda07f
Lim YeonWoo <yeonwoolim@naver.com>
2021-12-19 17:51:27 +0900

    code arrangement: completionRateScreen

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,7 @@
 export const theme = {
-    background: '#D8E0F2', //gray#BFBFBF
-    itemBackground: '#DBDBDB', //light gray#C5DAEA
-    main: '#083359', //black
-    text: '#707070', //whitegray
-    done: '#707070' //gray, check line
+    background: '#D8E0F2', 
+    itemBackground: '#DBDBDB', 
+    main: '#083359', 
+    text: '#707070', 
+    done: '#707070' 
 };
